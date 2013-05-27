e5be95a8099e4b532e3b0a9e9e90444386c74b0d
Diane Trout <diane@ghic.org>
2013-05-27 00:29:17 -0700

    Imported Upstream version 0.6.2

--- src/contactrunner.h
+++ src/contactrunner.h
@@ -1,18 +1,20 @@
 /*
+    KTp Contact Runner
     Copyright (C) 2012  Dan Vratil <dan@progdan.cz>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #ifndef KTPCONTACTRUNNER_H
@@ -23,23 +25,33 @@
 #include <Plasma/AbstractRunner>
 #include <KIcon>
 
-#include <KTp/Models/accounts-model.h>
-#include <KTp/Models/groups-model.h>
-#include <KTp/Models/accounts-filter-model.h>
-
 #include <TelepathyQt/AccountManager>
 
-class QAction;
+namespace KTp
+{
+    class GlobalPresence;
+}
 
 namespace Tp {
     class PendingOperation;
 }
+
+class QAction;
 
 class ContactRunner : public Plasma::AbstractRunner
 {
     Q_OBJECT
 
   public:
+    enum Capability {
+        AllCapabilitites = 0,
+        TextChatCapability,
+        AudioCallCapability,
+        VideoCallCapability,
+        DesktopSharingCapability,
+        FileTransferCapability
+    };
+
     ContactRunner( QObject *parent, const QVariantList& args );
     ~ContactRunner();
 
@@ -53,10 +65,18 @@ class ContactRunner : public Plasma::AbstractRunner
     void accountManagerReady(Tp::PendingOperation *operation);
 
   private:
-    bool hasCapability(const QModelIndex &contact, AccountsFilterModel::CapabilityFilterFlag capability) const;
+    bool hasCapability(const Tp::ContactPtr &contact, Capability capability) const;
 
-    AccountsModel *m_accountsModel;
+    void matchPresence(Plasma::RunnerContext &context);
+    void matchContacts(Plasma::RunnerContext &context);
+
+    void addPresenceMatch(Plasma::RunnerContext &context, Tp::ConnectionPresenceType presence,
+                          const QString &statusMessage);
+
+    KTp::GlobalPresence *m_globalPresence;
     Tp::AccountManagerPtr m_accountManager;
+
+    bool m_loggerDisabled;
 };
 
 K_EXPORT_PLASMA_RUNNER(ktp_contacts, ContactRunner)
